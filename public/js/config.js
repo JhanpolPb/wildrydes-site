90f166ccb885637608719cb4019fcf16c41e971b
Polsito <jhanpolpb840@gmail.com>
2026-08-02 13:37:12 -0500

    config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,19 @@
 window._config = {
+
     cognito: {
-        userPoolId: 'us-east-2_Cr5GjPBec',
-        userPoolClientId: '7mrq7fb0mmsqm6gv766s4j6hae',
-        region: 'us-east-2'
-    },
-    api: {
-        invokeUrl: ''
-    }
+
+        userPoolId: 'us-east-2_okcg4Dz3u', // e.g. us-east-2_uXboG5pAb         
+
+        userPoolClientId: '7je7em8he82mv13iq3ea6u9lcu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+
+        region: 'us-east-2' // e.g. us-east-2 
+
+    }, 
+
+    api: { 
+
+        invokeUrl: 'https://nven4jtk24.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
 };
